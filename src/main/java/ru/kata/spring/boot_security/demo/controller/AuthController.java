c4bfe6f7d3e93720427efac3360f21c6a716edf5
Shavgaev <131887575+Shavgaev@users.noreply.github.com>
2023-08-15 10:54:44 +0300

    исправил

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AuthController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AuthController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(name = "/login")
 public class AuthController {
+    @RequestMapping(name = "/login")
     public String loginPage() {
         return "login";
     }
